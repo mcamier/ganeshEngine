478b240f7dca19cdc89cce52e2b01d9a2f6ec3a2
Mickaël C <Camier.mickael@gmail.com>
2016-05-31 09:32:34 +0200

    Update main.cpp

--- ganeshEngineDemo/Source/main.cpp
+++ ganeshEngineDemo/Source/main.cpp
@@ -9,13 +9,11 @@ using namespace ganeshEngine;
 
 int main() {
 	Application::Initialize();
-    /** After application initialization all singletons used by the engine
-     *  are created and initialized, so all engine's customization, plugins
-     *  addition should take place here, before the start of the main loop
-     */
-    //Application::RunLoop();
-	Frustum f = Frustum(16.0f/9.0f, 80, 1.0f, 1000.0f);
-
-dew    Application::Destroy();
+	/** After application initialization all singletons used by the engine
+	 *  are created and initialized, so all engine's customization, plugins
+	 *  addition should take place here, before the start of the main loop
+	 */
+	Application::RunLoop();
+	Application::Destroy();
     return 0;
 }
